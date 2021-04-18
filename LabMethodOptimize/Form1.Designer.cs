255bdfa6dc18935dd60ccf6357577ad4993443ab
GlebHmarsky <hmarsky.gleb@yandex.ru>
2021-04-18 15:34:38 +0300

    Add menu and tab control

--- LabMethodOptimize/Form1.Designer.cs
+++ LabMethodOptimize/Form1.Designer.cs
@@ -37,8 +37,23 @@ namespace LabMethodOptimize
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.numericUpDownColumn = new System.Windows.Forms.NumericUpDown();
+            this.tableInitButton = new System.Windows.Forms.Button();
+            this.numericUpDownRow = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.SimpleTable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColumn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // SimpleTable
@@ -47,15 +62,17 @@ namespace LabMethodOptimize
             this.SimpleTable.AllowUserToDeleteRows = false;
             this.SimpleTable.AllowUserToResizeColumns = false;
             this.SimpleTable.AllowUserToResizeRows = false;
+            this.SimpleTable.BackgroundColor = System.Drawing.SystemColors.Control;
             this.SimpleTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.SimpleTable.Location = new System.Drawing.Point(12, 12);
+            this.SimpleTable.Location = new System.Drawing.Point(347, 26);
             this.SimpleTable.Name = "SimpleTable";
-            this.SimpleTable.Size = new System.Drawing.Size(445, 248);
+            this.SimpleTable.RowHeadersWidth = 80;
+            this.SimpleTable.Size = new System.Drawing.Size(661, 373);
             this.SimpleTable.TabIndex = 0;
             // 
             // buttonAddRow
             // 
-            this.buttonAddRow.Location = new System.Drawing.Point(491, 79);
+            this.buttonAddRow.Location = new System.Drawing.Point(18, 57);
             this.buttonAddRow.Name = "buttonAddRow";
             this.buttonAddRow.Size = new System.Drawing.Size(144, 47);
             this.buttonAddRow.TabIndex = 1;
@@ -65,7 +82,7 @@ namespace LabMethodOptimize
             // 
             // buttonDeletRow
             // 
-            this.buttonDeletRow.Location = new System.Drawing.Point(491, 151);
+            this.buttonDeletRow.Location = new System.Drawing.Point(18, 129);
             this.buttonDeletRow.Name = "buttonDeletRow";
             this.buttonDeletRow.Size = new System.Drawing.Size(144, 53);
             this.buttonDeletRow.TabIndex = 2;
@@ -75,7 +92,7 @@ namespace LabMethodOptimize
             // 
             // buttonDelColumn
             // 
-            this.buttonDelColumn.Location = new System.Drawing.Point(641, 151);
+            this.buttonDelColumn.Location = new System.Drawing.Point(168, 129);
             this.buttonDelColumn.Name = "buttonDelColumn";
             this.buttonDelColumn.Size = new System.Drawing.Size(144, 53);
             this.buttonDelColumn.TabIndex = 4;
@@ -85,7 +102,7 @@ namespace LabMethodOptimize
             // 
             // buttonAddColumn
             // 
-            this.buttonAddColumn.Location = new System.Drawing.Point(641, 79);
+            this.buttonAddColumn.Location = new System.Drawing.Point(168, 57);
             this.buttonAddColumn.Name = "buttonAddColumn";
             this.buttonAddColumn.Size = new System.Drawing.Size(144, 47);
             this.buttonAddColumn.TabIndex = 3;
@@ -95,7 +112,7 @@ namespace LabMethodOptimize
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(491, 48);
+            this.textBox1.Location = new System.Drawing.Point(18, 26);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(130, 20);
             this.textBox1.TabIndex = 5;
@@ -103,7 +120,7 @@ namespace LabMethodOptimize
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(641, 48);
+            this.textBox2.Location = new System.Drawing.Point(168, 26);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(130, 20);
             this.textBox2.TabIndex = 6;
@@ -111,29 +128,143 @@ namespace LabMethodOptimize
             // 
             // numericUpDownColumn
             // 
-            this.numericUpDownColumn.Location = new System.Drawing.Point(641, 240);
+            this.numericUpDownColumn.Location = new System.Drawing.Point(168, 218);
             this.numericUpDownColumn.Name = "numericUpDownColumn";
             this.numericUpDownColumn.Size = new System.Drawing.Size(144, 20);
             this.numericUpDownColumn.TabIndex = 7;
             this.numericUpDownColumn.ValueChanged += new System.EventHandler(this.numericUpDownColumn_ValueChanged);
             // 
+            // tableInitButton
+            // 
+            this.tableInitButton.Location = new System.Drawing.Point(168, 286);
+            this.tableInitButton.Name = "tableInitButton";
+            this.tableInitButton.Size = new System.Drawing.Size(144, 91);
+            this.tableInitButton.TabIndex = 8;
+            this.tableInitButton.Text = "BUTT";
+            this.tableInitButton.UseVisualStyleBackColor = true;
+            this.tableInitButton.Click += new System.EventHandler(this.tableInitButton_Click);
+            // 
+            // numericUpDownRow
+            // 
+            this.numericUpDownRow.Location = new System.Drawing.Point(18, 218);
+            this.numericUpDownRow.Name = "numericUpDownRow";
+            this.numericUpDownRow.Size = new System.Drawing.Size(144, 20);
+            this.numericUpDownRow.TabIndex = 9;
+            this.numericUpDownRow.ValueChanged += new System.EventHandler(this.numericUpDownRow_ValueChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1119, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem1.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabPage3);
+            this.tabControl.Controls.Add(this.tabPage4);
+            this.tabControl.Location = new System.Drawing.Point(0, 27);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1119, 602);
+            this.tabControl.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabPage1.Controls.Add(this.SimpleTable);
+            this.tabPage1.Controls.Add(this.buttonAddRow);
+            this.tabPage1.Controls.Add(this.numericUpDownRow);
+            this.tabPage1.Controls.Add(this.buttonDeletRow);
+            this.tabPage1.Controls.Add(this.tableInitButton);
+            this.tabPage1.Controls.Add(this.buttonAddColumn);
+            this.tabPage1.Controls.Add(this.numericUpDownColumn);
+            this.tabPage1.Controls.Add(this.buttonDelColumn);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1111, 576);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1111, 576);
+            this.tabPage2.TabIndex = 3;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1111, 576);
+            this.tabPage3.TabIndex = 4;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1111, 576);
+            this.tabPage4.TabIndex = 5;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 476);
-            this.Controls.Add(this.numericUpDownColumn);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.buttonDelColumn);
-            this.Controls.Add(this.buttonAddColumn);
-            this.Controls.Add(this.buttonDeletRow);
-            this.Controls.Add(this.buttonAddRow);
-            this.Controls.Add(this.SimpleTable);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1119, 630);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.SimpleTable)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColumn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,6 +280,17 @@ namespace LabMethodOptimize
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.NumericUpDown numericUpDownColumn;
+        private System.Windows.Forms.Button tableInitButton;
+        private System.Windows.Forms.NumericUpDown numericUpDownRow;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
     }
 }
 
--- LabMethodOptimize/Form1.cs
+++ LabMethodOptimize/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,13 +23,13 @@ namespace LabMethodOptimize
             if (this.SimpleTable.Columns.Count > 0)
                 this.SimpleTable.Rows.Add();
         }
-
         private void buttonDeletRow_Click(object sender, EventArgs e)
         {
             if (this.SimpleTable.Rows.Count > 0)
                 this.SimpleTable.Rows.RemoveAt(this.SimpleTable.Rows.Count - 1);
 
         }
+
 
         private void buttonAddColumn_Click(object sender, EventArgs e)
         {
@@ -41,31 +42,34 @@ namespace LabMethodOptimize
 
         }
 
+
         private void numericUpDownColumn_ValueChanged(object sender, EventArgs e)
         {
             if (SimpleTable.Columns.Count < 1)
-            {                
-                this.SimpleTable.Columns.Add(1.ToString(), "C");                
+            {
+                this.SimpleTable.Columns.Add(1.ToString(), "C");
             }
 
-            
+
             if ((int)numericUpDownColumn.Value + 1 > SimpleTable.Columns.Count)
             {
-                for (int i = SimpleTable.Columns.Count ; i < (int)numericUpDownColumn.Value + 1; i++)
+                for (int i = SimpleTable.Columns.Count; i < (int)numericUpDownColumn.Value + 1; i++)
                 {
                     this.SimpleTable.Columns.Add(i.ToString(), "C" + i);
                 }
             }
-            else 
+            else
             {
                 for (int i = SimpleTable.Columns.Count; i > (int)numericUpDownColumn.Value + 1; i--)
                 {
                     if (i > 0)
                         this.SimpleTable.Columns.RemoveAt(i - 1);
                 }
-                if(numericUpDownColumn.Value == 0)
+                if (numericUpDownColumn.Value == 0)
                 {
+                    numericUpDownRow.Value = 0;
                     this.SimpleTable.Columns.RemoveAt(0);
+
                 }
             }
 
@@ -73,10 +77,84 @@ namespace LabMethodOptimize
             {
 
                 column.HeaderText = String.Concat("C",
-                    (column.Index+1).ToString());
+                    (column.Index + 1).ToString());
             }
-            if (SimpleTable.Columns.Count != 0) 
-                SimpleTable.Columns[SimpleTable.Columns.Count-1].HeaderText = "C";
+            if (SimpleTable.Columns.Count != 0)
+                SimpleTable.Columns[SimpleTable.Columns.Count - 1].HeaderText = "C";
+        }
+        private void numericUpDownRow_ValueChanged(object sender, EventArgs e)
+        {
+
+            if ((int)numericUpDownRow.Value > SimpleTable.Rows.Count)
+                this.SimpleTable.Rows.Add((int)numericUpDownRow.Value - SimpleTable.Rows.Count);
+            else
+            {
+
+                for (int i = SimpleTable.Rows.Count; i > (int)numericUpDownRow.Value; i--)
+                {
+                    if (i > 0)
+                        this.SimpleTable.Rows.RemoveAt(i - 1);
+                }
+                //if (numericUpDownRow.Value == 0)
+                //{
+                //    this.SimpleTable.Rows.RemoveAt(0);
+                //}
+            }
+
+            foreach (DataGridViewRow row in SimpleTable.Rows)
+            {
+
+                row.HeaderCell.Value = String.Concat("f",
+                    (row.Index + 1).ToString());
+            }
+            
+            //if (SimpleTable.Rows.Count != 0)
+            //    SimpleTable.Rows[SimpleTable.Rows.Count - 1].HeaderCell.Value = "Z";
+        }
+        private void tableInitButton_Click(object sender, EventArgs e)
+        {
+
+            string path = @"R:\Programming\C#\LabMethodOptimize\LabMethodOptimize\MethodOptimize.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string[] str = sr.ReadLine().Split(' ');
+                    // TODO:  Проверки на дурака
+                    uint RowCount, ColumnCount;
+                    RowCount = (uint)Convert.ToInt32(str[0]);
+                    ColumnCount = (uint)Convert.ToInt32(str[1]);
+                    //Solution = new FractionGausMethod(RowCount, ColumnCount);
+
+                    for (int i = 0; i < RowCount; i++)
+                    {
+                        str = sr.ReadLine().Split(' ');
+                        //UNDONE Проверки на дурака 2
+                        for (int g = 0; g < ColumnCount; g++)
+                        {
+                            //Solution.Matrix[i][g] = Convert.ToInt32(str[g]);
+                            SimpleTable.Rows[i].Cells[g].Value = Convert.ToInt32(str[g]);
+                        }
+                    }
+
+                    str = sr.ReadLine().Split(' ');
+                    for (int i = 0; i < RowCount; i++)
+                    {
+                        //Solution.RightPart[i] = Convert.ToInt32(str[i]);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+
+
+            //Solution.SolveMatrix(); // решаем СЛАУ
+            //Console.WriteLine(Solution.ToString());// Печатаем результат
+
+
         }
     }
 
